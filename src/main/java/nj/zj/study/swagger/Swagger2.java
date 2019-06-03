7fb316c67fe4a46951383203b2188df163a28f33
YuLoving <974456795@qq.com>
2019-06-03 10:18:27 +0800

    添加swagger配置

---
+++ src/main/java/nj/zj/study/swagger/Swagger2.java
@@ -0,0 +1,71 @@
+package nj.zj.study.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**  
+
+* <p>Description: 
+	Swagger2配置类
+ * 在与spring boot集成时，放在与Application.java同级的目录下。
+ * 通过@Configuration注解，让Spring来加载该类配置。
+ * 再通过@EnableSwagger2注解来启用Swagger2。
+* </p>  
+
+* @author ZTY  
+
+* @date 2019年6月3日  
+
+*/
+@Configuration 
+@EnableSwagger2
+public class Swagger2 {
+	
+	/**
+     * 创建API应用
+     * apiInfo() 增加API相关信息
+     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
+     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
+     * 本例采用指定扫描的包路径来定义，Swagger会扫描该包下所有Controller定义的API，
+     * 并产生文档内容（除了被@ApiIgnore指定的请求）。添加文档内容
+     * @return
+     */
+	
+	
+	
+	@Bean
+	public Docket createRestApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("nj.zj.study.controller"))
+				.paths(PathSelectors.any())
+				.build();
+				
+				
+	}
+	
+	 /**
+     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
+     * 访问地址：http://项目实际地址/swagger-ui.html
+     * @return
+     */
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Spring Boot中使用Swagger2构建RESTful APIs")  //标题（一直是写该项目的名称）
+				.description("主要是测试springboot与多个技术栈的集成使用")
+				.termsOfServiceUrl("")//超链接
+				.contact("ZTY")//联系人
+				.version("1.0")
+				.build();
+	}
+
+}
