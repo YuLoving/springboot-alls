4c9245109628dab3e91474278af4b7e00e056652
zhaotianyu <zhaotianyu@91yhai.com>
2019-06-26 17:42:35 +0800

    添加工具类

--- src/main/java/nj/zj/study/utils/JacksonConfig.java
+++ src/main/java/nj/zj/study/utils/JacksonConfig.java
@@ -34,6 +34,14 @@ public class JacksonConfig {
 	    @ConditionalOnMissingBean(ObjectMapper.class)
 	    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
 	        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+	        // 通过该方法对mapper对象进行设置，所有序列化的对象都将按改规则进行系列化  
+	        // Include.Include.ALWAYS 默认  
+	        // Include.NON_DEFAULT 属性为默认值不序列化  
+	        // Include.NON_EMPTY 属性为 空（""） 或者为 NULL 都不序列化，则返回的json是没有这个字段的。这样对移动端会更省流量  
+	        // Include.NON_NULL 属性为NULL 不序列化  
+	        
+	        
+	     // 字段保留，将null值转为""  
 	        objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
 	            @Override
 	            public void serialize(Object o, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
